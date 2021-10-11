97efab93944e4a006220e75c10be5e5b2e683bd5
J. Zebedee <zebedee@code.gripe>
2021-10-11 01:10:53 -0500

    Update IRelayInfoCommand to WCT MVVM

--- WinDirStat.Net.Wpf.Single/Commands/IRelayInfoCommand.cs
+++ WinDirStat.Net.Wpf.Single/Commands/IRelayInfoCommand.cs
@@ -1,29 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Toolkit.Mvvm.Input;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 using System.Windows.Media;
-using WinDirStat.Net.ViewModel;
-using WinDirStat.Net.ViewModel.Commands;
 using WinDirStat.Net.Wpf.Input;
 
 namespace WinDirStat.Net.Wpf.Commands {
-	public interface IRelayInfoCommand : IRelayCommandBase, INotifyPropertyChanged {
+    public interface IRelayInfoCommand : IRelayCommand, INotifyPropertyChanged {
 
-		#region Properties
+        #region Properties
 
-		/// <summary>Gets or sets the UI specific info for the command.</summary>
-		RelayInfo Info { get; set; }
-		/// <summary>Gets the display text for the command.</summary>
-		string Text { get; }
-		/// <summary>Gets the display icon for the command.</summary>
-		ImageSource Icon { get; }
-		/// <summary>Gets the input gesture for the command.</summary>
-		AnyKeyGesture InputGesture { get; }
+        /// <summary>Gets or sets the UI specific info for the command.</summary>
+        RelayInfo Info { get; set; }
+        /// <summary>Gets the display text for the command.</summary>
+        string Text { get; }
+        /// <summary>Gets the display icon for the command.</summary>
+        ImageSource Icon { get; }
+        /// <summary>Gets the input gesture for the command.</summary>
+        AnyKeyGesture InputGesture { get; }
 
-		#endregion
-	}
+        #endregion
+    }
 }
