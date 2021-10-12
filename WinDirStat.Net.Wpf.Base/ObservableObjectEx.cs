6b1d78c3bb9e0a8aa715316c394e72fd24528926
J. Zebedee <zebedee@code.gripe>
2021-10-12 11:07:28 -0500

    Update ObservableObjectEx to WCT MVVM

--- WinDirStat.Net.Wpf.Base/ObservableObjectEx.cs
+++ WinDirStat.Net.Wpf.Base/ObservableObjectEx.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using GalaSoft.MvvmLight;
 
 namespace WinDirStat.Net {
-	/// <summary>An observable object with extra raise property changed methods.</summary>
-	public class ObservableObjectEx : ObservableObject {
+    /// <summary>An observable object with extra raise property changed methods.</summary>
+    public class ObservableObjectEx : ObservableObject {
 
 		/// <summary>Raises the property as changed if the condition is true.</summary>
 		/// 
 		/// <param name="condition">The condition for raising the changed event.</param>
 		/// <param name="propertyName">The name of the property.</param>
 		/// <returns>True if the property was changed.</returns>
-		protected bool RaisePropertyChangedIf(bool condition, [CallerMemberName] string propertyName = null) {
-			RaisePropertyChanged(propertyName);
+		protected bool OnPropertyChangedIf(bool condition, [CallerMemberName] string propertyName = null) {
+            //FIXME: this never checks the condition...
+			OnPropertyChanged(propertyName);
 			return condition;
 		}
 	}
